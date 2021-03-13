86124cf0ff1d2c63c9a48fd3c336e04e1053ae00
kevin <kevinhellman94@gmail.com>
2021-03-13 10:47:42 -0500

    Inital Fixes to get site working

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   development: {
-    username: process.env.SEQUELIZE_USER,
-    password: process.env.SEQUELIZE_PASSWORD,
+    username: process.env.SEQUELIZE_USER || 'root',
+    password: process.env.SEQUELIZE_PASSWORD || 'Threshmain',
     database: 'project2_dev',
     dialect: 'mysql',
-    host: process.env.SEQUELIZE_HOST,
+    host: 'localhost',
     port: 3306
   },
   test: {
-    username: process.env.TU,
+    username: process.env.TU || null,
     password: process.env.TP || null,
     database: 'project2_test',
     host: 'localhost',
