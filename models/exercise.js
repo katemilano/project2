8a08afcd0932ba2396e8439cf5a6c023f6cfd393
Matt Autrey <mautrey17@gmail.com>
2021-03-13 10:56:54 -0500

    eslint changes

--- models/exercise.js
+++ models/exercise.js
@@ -1,14 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
-    const exercise = sequelize.define('exercise', {
-        name: DataTypes.STRING,
-        equipment: DataTypes.STRING,
-        exercise_type: DataTypes.STRING,
-        muscle_major: DataTypes.STRING,
-        muscle_minor: DataTypes.STRING,
-        example_link: DataTypes.STRING,
-        notes: DataTypes.STRING,
-        modifications: DataTypes.STRING
-    })
+  const exercise = sequelize.define('exercise', {
+    name: DataTypes.STRING,
+    equipment: DataTypes.STRING,
+    exercise_type: DataTypes.STRING,
+    muscle_major: DataTypes.STRING,
+    muscle_minor: DataTypes.STRING,
+    example_link: DataTypes.STRING,
+    notes: DataTypes.STRING,
+    modifications: DataTypes.STRING
+  });
 
-    return exercise;
-}
+  return exercise;
+};
