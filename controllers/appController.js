27af9fd17d94f91fcf666c39cad546253ef109bf
Matt Autrey <mautrey17@gmail.com>
2021-03-15 19:54:02 -0400

    linting fix for first operator push

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,3 +1,4 @@
+const Sequelize = require('sequelize');
 module.exports = function (db) {
   return {
     // Get all examples
@@ -27,14 +28,14 @@ module.exports = function (db) {
     // get specific exercises
     getSpecificExercises: (req, res) => {
       // const filter = {
-      //   muscle_major: req.body.muscle,
+      //  muscle_major: req.body.muscle,
       //   exercise_type: req.body.type,
       //   equipment: req.body.equipment
       // };
 
       db.Exercise.findAll({
         where: {
-          [sequelize.Op.or]: [{muscle_major: req.body.muscle},{exercise_type: req.body.type}]
+          [Sequelize.Op.or]: [{ muscle_major: req.body.muscle }, { exercise_type: req.body.type }]
         }
       }).then(function (exercises) {
         res.json(exercises);
