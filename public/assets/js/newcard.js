ea7c7e93a22eaea301f3586c9fb066a22443a2b9
kevin <kevinhellman94@gmail.com>
2021-03-16 13:41:33 -0400

    Appending all elements

--- public/assets/js/newcard.js
+++ public/assets/js/newcard.js
@@ -5,6 +5,37 @@ const newCard = (exercise) => {
   img.setAttribute('class', 'exercise-example');
   div.append(img);
 
+  const name = document.createElement('h4');
+  name.innerText = exercise.name;
+  div.append(name);
+
+  const hr = document.createElement('hr');
+  hr.setAttribute('class','card-line');
+  div.append(hr);
+
+  const equipment = document.createElement('p');
+  equipment.innerText = exercise.equipment;
+  div.append(equipment);
+
+  const type = document.createElement('p');
+  type.innerText = exercise.type;
+  div.append(type);
+
+  const major = document.createElement('p');
+  major.innerText = exercise.major;
+  div.append(major);
+
+  const minor = document.createElement('p');
+  minor.innerText = exercise.minor;
+  div.append(minor);
+
+  const notes = document.createElement('p');
+  notes.innerText = exercise.notes;
+  div.append(notes);
+
+  const modification = document.createElement('p');
+  modification.innerText = exercise.modification;
+  div.append(modification);
 };
 
 module.exports = newCard();
