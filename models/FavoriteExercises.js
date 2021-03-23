915f2f5329c76dbc925585122005fc2a92aff757
Matt Autrey <mautrey17@gmail.com>
2021-03-23 18:07:28 -0400

    added id to FavoriteExercises table

--- models/FavoriteExercises.js
+++ models/FavoriteExercises.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   const FavoriteExercises = sequelize.define('FavoriteExercises', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
     UserId: DataTypes.STRING,
     ExerciseId: DataTypes.STRING
   });
